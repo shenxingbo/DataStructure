6f907377445867920e27d461d36a00f3a9d84f99
shenxingbo <2461152330@qq.com>
2017-08-28 14:57:06 +0800

    finish queue.h

---
+++ queue/array/queue.h
@@ -0,0 +1,26 @@
+
+struct QueueRecord;
+typedef struct QueueRecord *Queue;
+typedef int ElementType ; 
+
+int IsEmpty(Queue q);
+int IsFull(Queue q);
+Queue CreateQueue(int MaxElements); //指示的是栈的深度
+void DisposeQueue(Queue q);
+void MakeEmpty(Queue q);
+void EnQueue(ElementType x,Queue q);
+ElementType Front(Queue q);
+void Dequeue(Queue q);
+ElementType FrontAndDequeue(Queue q);
+
+struct QueueRecord{
+	int capacity;
+	int front;
+	int rear;
+	int size;
+	ElementType *array;
+};
+
+
+
+
