bcfb950a2d65e7f718785252e5bd76f9cf20e012
shenxingbo <2461152330@qq.com>
2017-10-23 21:23:25 +0800

    这是基础的并查集

---
+++ disjointset/basic/disjsets.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+void Initialize(DisjSet S) {
+	int i;
+
+	for ( i = NumSets; i > 0; i--) {
+		S[i] = 0;
+	}
+} 
+
+
+void SetUnion(DisjSet S, SetType Root1, SetType Root2) {
+	S[Root2] = Root1; 
+}
+
+SetType Find(ElementType X, DisjSet S) {
+	if (S[X] <= 0) {
+		return X;  //·µ»Ø¸ùÔªËØ 
+	}else {
+		return Find(S[X],S);
+	} 
+}
